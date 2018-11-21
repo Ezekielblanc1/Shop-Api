4d8aadf403764aceb5b6eab3e9124082cfb999c1
Ezekiel blanc <ezekielblanc1@gmail.com>
2018-11-21 14:04:37 +0100

    set up product routes

---
+++ api/routes/products.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /products'
+  })
+})
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling POST requests to /products'
+  })
+})
+
+router.get('/:id', (req, res, next)=>{
+  const id = req.params.id;
+  if(id === 'node') {
+    res.status(200).json({
+      message: 'You can learn node',
+      id: id
+    })
+  } else{
+    res.status(200).json({
+      message: 'You passed an ID'
+    })
+  }
+})
+router.patch('/:id', (req, res, next) => {
+  res.status(200).json({
+    message: 'Updated Product'
+  })
+})
+
+router.delete('/:id', (req, res,next) => {
+  res.status(200).json({
+    message: 'Deleted product'
+  })
+})
+
+module.exports = router
