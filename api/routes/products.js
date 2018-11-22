56b4d439f533373a0080908737ebcf4025c19537
Ezekiel blanc <ezekielblanc1@gmail.com>
2018-11-22 20:15:23 +0100

    Restructured the routes to be able to query the database

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,42 +1,92 @@
 const express = require('express');
 const router = express.Router();
 
+const Product = require('../models/product')
+
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /products'
-  })
+  Product.find()
+    .exec()
+    .then((allProducts) => {
+      console.log(allProducts)
+      res.status(200).json(allProducts)
+    })
+    .catch(err => {
+      res.status(404).json({
+        error: err.message
+      })
+    })
 })
 
 router.post('/', (req, res, next) => {
-  const product = req.body.name
-  res.status(200).json({
-    message: 'Handling POST requests to /products'
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price
+  });
+  product
+  .save()
+  .then((result) => {
+    if(result){
+      res.status(200).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: result
+      })
+    } else{
+      res.status(404).json({message: 'No resource was found with the given ID'})
+    }
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json({
+      error: err
+    })
   })
 })
 
 router.get('/:id', (req, res, next)=>{
   const id = req.params.id;
-  if(id === 'node') {
-    res.status(200).json({
-      message: 'You can learn node',
-      id: id
+  Product.findById(id)
+  .exec()
+  .then((foundData) => {
+    if(foundData){
+      console.log(foundData)
+      res.status(200).json({
+      docs: foundData
     })
-  } else{
-    res.status(200).json({
-      message: 'You passed an ID'
+    } else{
+      res.status(404).json({message: 'No resource available with the given ID'})
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      error: err
     })
-  }
-})
-router.patch('/:id', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated Product'
   })
 })
 
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id
+  let {name , price} = req.body;
+  Product.findByIdAndUpdate(id,{ name , price },{new:true})
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        result
+      })
+    })
+})
+
 router.delete('/:id', (req, res,next) => {
-  res.status(200).json({
-    message: 'Deleted product'
+  const id = req.params.id
+ Product.remove({ _id : id})
+  .exec()
+  .then((result) =>{
+    res.status(200).json(result)
   })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({error: err})
+  });
 })
 
 module.exports = router
