9935c219d3d1bba2672a67fcf0f71d1e14e9834d
Ezekiel blanc <ezekielblanc1@gmail.com>
2018-11-22 20:10:40 +0100

    Added in middleware to handle CORS

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/shop-api', {useNewUrlParser: true})
+.then(() => console.log('Connected to database'))
+.catch(err => console.error('Could not connect to database')); 
+            
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders')
@@ -9,6 +15,19 @@ const orderRoutes = require('./api/routes/orders')
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
+//Handling CORS
+
+app.use((req, res, next)=> {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+  }
+  next();
+})
+
+
 app.use('/products', productRoutes)
 
 app.use('/orders', orderRoutes) 
