0aba98fa2453f8422867021203b0695e3673e9d0
Ezekiel blanc <ezekielblanc1@gmail.com>
2018-11-27 14:19:10 +0100

    Added in the multer package to store files

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const router = express.Router();
-
+const mongoose = require('mongoose')
+const multer = require('multer')
 const Product = require('../models/product')
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, './uploads')
+  },
+  filename: function(req, file, cb){
+    cb(null, new Date().toISOString() + file.originalname)
+  }
+})
+//filter to accept certain mimetype
+
+const filefilter = (req, file, cb) =>{
+ if(file.mimetype === 'image/jpeg' ||file.mimetype === 'image/png'){
+   cb(null, true)
+ }
+ cb(null,true)
+}
+//Setting up a filter based on some limits and to accept a specified file size
+const upload = multer(
+  {storage: storage,
+   limits: {
+   fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: filefilter
+})
 
 router.get('/', (req, res, next) => {
   Product.find()
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then((allProducts) => {
       const response = {
@@ -15,6 +40,7 @@ router.get('/', (req, res, next) => {
             name: product.name,
             price: product.price,
             _id: product._id,
+            productImage: product.productImage,
             request: {
               type: 'GET',
               url: 'http://localhost:8080/products/' + product._id 
@@ -31,10 +57,12 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/',upload.single('productImage'), (req, res, next) => {
+  console.log(req.file)
   const product = new Product({
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
+    productImage: req.file.path
   });
   product
   .save()
@@ -67,7 +95,7 @@ router.post('/', (req, res, next) => {
 router.get('/:id', (req, res, next)=>{
   const id = req.params.id;
   Product.findById(id)
-  .select('name price _id')
+  .select('name price _id productImage')
   .exec()
   .then((foundData) => {
     if(foundData){
